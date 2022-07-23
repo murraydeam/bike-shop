45105202006c5b2c7c6fa55c4f4516f8e38e45f9
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-07-22 23:43:04 -0400

    Redux landing page complete

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,19 +1,25 @@
-export const productListReducer = (state = {produts:[]}, action) => {
-    switch(action.type){
-        case 'PRODUCT_LIST_REQUEST':
-            return {loading:true, products:[]}
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstants";
 
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
 
-        case 'PRODUCT_LIST_SUCCESS':
-            return {loading: false, products:action.payload}
-    
-        case 'PRODUCT_LIST_FAIL':
-            return {loading: false, error:action.payload}
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
 
-        default:
-            return state
-        
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
 
-    }
-
-}
+    default:
+      return state;
+  }
+};
