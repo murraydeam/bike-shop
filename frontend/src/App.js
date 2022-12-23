0bd2c29d1abc15bc03a6afdbc372df8c6acd97d6
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-12-23 11:54:31 -0500

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Main</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
