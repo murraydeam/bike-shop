4728b495f2382823c0dfc2879d53526eba3a6264
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-12-31 12:15:38 -0500

    product screen now shows items in stock along with a brief description

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
 
@@ -8,7 +8,61 @@ const ProductScreen = () => {
   const match = useParams();
   const product = products.find((p) => p.id == match.id);
 
-  return <div>{product.name}</div>;
+  return (
+    <div className="productScreenContainer">
+      <Link to={"/"} className="btn btn-dark my-3">
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews}`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>{product.price}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>
+                <strong>Description : </strong>
+                {product.descriptionLong}
+              </p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price:</Col>
+                <Col>
+                  <strong>{product.price}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status:</Col>
+                <Col>
+                  <strong>In Stock</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ProductScreen;
